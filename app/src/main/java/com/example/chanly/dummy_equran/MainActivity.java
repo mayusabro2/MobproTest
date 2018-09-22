f7fbfcac9cc027d863fe5aa13e1ae521f10d2da7
Chanly Septian <Chanlyseptian17@gmail.com>
2018-09-22 16:49:09 +0700

    asdasd

--- app/src/main/java/com/example/chanly/dummy_equran/MainActivity.java
+++ app/src/main/java/com/example/chanly/dummy_equran/MainActivity.java
@@ -28,7 +28,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     RequestQueue mRequestQueue;
     ArrayList<Surah> mListSurah;
-    SurahAdapter mSurahAdapter;aaaaaa
+    SurahAdapter mSurahAdapter;
     LinearLayout mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
